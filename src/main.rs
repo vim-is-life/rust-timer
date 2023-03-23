1c6022a32598d473ae226b780287a308a5bac4d6
vim-is-life <78273932+vim-is-life@users.noreply.github.com>
2023-03-23 05:48:59 -0500

    added two structs to help dealing with passed time
    The passed time struct records the value passed on the command line and
    has a parse method that I use to make a CountdownTime struct, which
    holds the minutes and seconds as well as the time in only seconds.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@
 
 use clap::Parser;
 use std::io::Write;
+use std::num::ParseIntError;
 use std::time::Instant;
 
 // Define the cli args
@@ -17,7 +18,72 @@ use std::time::Instant;
 #[command(author = "vim-is-life", about = "A CLI timer program.")]
 struct Args {
     /// The time, currently in seconds, to count down from.
-    time: u64,
+    // time: u64,
+
+    /// The time to count down from. Accepted syntax: 15m, 1min, 3secs, 4s.
+    /// Will default to seconds if no suffix is passed.
+    time_str: String,
+}
+
+/// Struct to represent the passed time before parsing
+#[derive(Debug)]
+struct PassedTime {
+    given_time: String,
+}
+
+impl PassedTime {
+    fn parse(&self) -> Result<CountdownTime, ParseIntError> {
+        let test = (
+            self.given_time.contains("min"),
+            self.given_time.contains('m'),
+            self.given_time.contains("sec"),
+            self.given_time.contains('s'),
+        );
+        match test {
+            (true, _, _, _) => {
+                let mins: u64 = self.given_time.split("min").collect::<String>().parse()?;
+                Ok(CountdownTime {
+                    mins_and_secs: (mins, 0),
+                    complete_secs: mins * 60,
+                })
+            }
+            (_, true, _, _) => {
+                let mins: u64 = self.given_time.split('m').collect::<String>().parse()?;
+                Ok(CountdownTime {
+                    mins_and_secs: (mins, 0),
+                    complete_secs: mins * 60,
+                })
+            }
+            (_, _, true, _) => {
+                let secs: u64 = self.given_time.split("sec").collect::<String>().parse()?;
+                Ok(CountdownTime {
+                    mins_and_secs: (0, secs),
+                    complete_secs: secs,
+                })
+            }
+            (_, _, _, true) => {
+                let secs: u64 = self.given_time.split('s').collect::<String>().parse()?;
+                Ok(CountdownTime {
+                    mins_and_secs: (0, secs),
+                    complete_secs: secs,
+                })
+            }
+            _ => {
+                let secs: u64 = self.given_time.parse()?;
+                Ok(CountdownTime {
+                    mins_and_secs: (0, secs),
+                    complete_secs: secs,
+                })
+            }
+        }
+    }
+}
+
+/// Struct to represent the time to count down from
+#[derive(Debug)]
+struct CountdownTime {
+    mins_and_secs: (u64, u64),
+    complete_secs: u64,
 }
 
 // We make the parameter a u64 because the current time will come as a u64.
@@ -48,5 +114,11 @@ fn countdown(end_time: u64) {
 fn main() {
     let args = Args::parse();
 
-    countdown(args.time);
+    // countdown(args.time);
+    let time = (PassedTime {
+        given_time: args.time_str,
+    })
+    .parse()
+    .expect("Unable to parse the time correctly.");
+    countdown(time.complete_secs);
 }
